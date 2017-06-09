095acf072ef93ab41ceafdcb321c4623da5b7de0
AQUIN0S <aquinos@acyonix.com>
2017-06-09 22:55:40 +1200

    Added a lot more functionality to the app, making it so that there are now two teams (as in an actual basketball game), and both teams' scores can be updated by adding either 3, 2 or 1 point to the score. A new launcher icon was also chosen, so that's cool too :D

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,27 +7,38 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void noteThreePointer(View view) {
 
-        displayForTeamA(3);
+    /**
+     * Scoring methods for Team A
+     */
+    public void threePointerTeamA(View view) {
+
+        this.scoreTeamA += 3;
+        displayForTeamA(this.scoreTeamA);
 
     }
 
-    public void noteBasket(View view) {
+    public void basketTeamA(View view) {
 
-        displayForTeamA(2);
+        this.scoreTeamA += 2;
+        displayForTeamA(this.scoreTeamA);
 
     }
 
-    public void noteFreeThrow(View view) {
+    public void freeThrowTeamA(View view) {
 
-        displayForTeamA(1);
+        this.scoreTeamA += 1;
+        displayForTeamA(this.scoreTeamA);
 
     }
 
@@ -37,4 +48,37 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA.setText(String.valueOf(score));
 
     }
+
+
+    /**
+     * Scoring methods for Team B
+     */
+    public void threePointerTeamB(View view) {
+
+        this.scoreTeamB += 3;
+        displayForTeamB(this.scoreTeamB);
+
+    }
+
+    public void basketTeamB(View view) {
+
+        this.scoreTeamB += 2;
+        displayForTeamB(this.scoreTeamB);
+
+    }
+
+    public void freeThrowTeamB(View view) {
+
+        this.scoreTeamB += 1;
+        displayForTeamB(this.scoreTeamB);
+
+    }
+
+    private void displayForTeamB(int score) {
+
+        TextView scoreTeamB = (TextView) findViewById(R.id.team_B_score);
+        scoreTeamB.setText(String.valueOf(score));
+
+    }
+
 }
